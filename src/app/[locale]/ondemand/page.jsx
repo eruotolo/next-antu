b0a6f781aeed1999edea9dd5a2808de2e5122feb
Edgardo Ruotolo <edgardoruotolo@gmail.com>
2024-12-09 01:10:49 -0300

    Task: Creating FAQ Date: 09-12-2024 Version: 1.3

--- src/app/[locale]/ondemand/page.jsx
+++ src/app/[locale]/ondemand/page.jsx
@@ -1,4 +1,15 @@
 import { useTranslations } from 'next-intl';
+import { Link } from '@/i18n/routing';
+import Image from 'next/image';
+import FagsComponent from '@/components/Faqs/FaqsComponent';
+
+const item01 = '/ondemand/1_antu.svg';
+const item02 = '/ondemand/2_antu.svg';
+const item03 = '/ondemand/3_antu.svg';
+const imgClasesOne = '/ondemand/barre_antu.webp';
+const imgClasesTwo = '/ondemand/pilates_antu.webp';
+const imgClasesThree = '/ondemand/signature_antu.webp';
+const imgEnjoy = '/ondemand/clases_signature_antu.webp';
 
 export default function OnDemandPage() {
     const t = useTranslations('OnDemandPage');
@@ -23,7 +34,7 @@ export default function OnDemandPage() {
                 </div>
             </section>
 
-            <section className="sm:py-[100px]">
+            <section className="sm:pt-[100px]">
                 <div className="container mx-auto">
                     <div className="flex flex-col items-center">
                         <h1 className="font-marcellus font-normal text-azul sm:text-[70px] sm:leading-[75px] sm:tracking-[0.02em]">
@@ -36,7 +47,117 @@ export default function OnDemandPage() {
                         </div>
                     </div>
 
-                    <div className="section-clases-ondemand flex items-center justify-center sm:mt-[100px] sm:h-[440px] sm:flex-col"></div>
+                    <div className="section-clases-ondemand flex items-center justify-center sm:mt-[100px] sm:h-[440px] sm:flex-col">
+                        <h3 className="font-marcellus font-normal uppercase text-beige sm:mb-[20px] sm:text-[27px] sm:tracking-[0.1em]">
+                            {t('freeClass')}
+                        </h3>
+                        <Link
+                            href="/ondemand"
+                            className="flex h-[54px] w-[250px] items-center justify-center rounded-[26px] border-[2px] border-rosa bg-rosa font-marcellus font-normal uppercase text-beige hover:bg-rosa hover:opacity-50 sm:m-[10px] sm:text-[23px]"
+                        >
+                            {t('btnFreeClass')}
+                        </Link>
+                    </div>
+
+                    <div className="container mx-auto flex items-center justify-center sm:my-[100px] sm:flex-col">
+                        <Image src={item01} alt={'Item Uno'} width={75} height={63} />
+                        <h4 className="font-manrope font-medium uppercase text-gris sm:mt-[20px] sm:text-[27px] sm:tracking-[0.25em]">
+                            {t('itemOne')}
+                        </h4>
+
+                        <div className="gap-10 sm:my-[80px] sm:grid sm:grid-cols-3">
+                            <div className="col-span-1">
+                                <Image
+                                    src={imgClasesOne}
+                                    alt={'Clases Uno'}
+                                    width={320}
+                                    height={320}
+                                />
+                            </div>
+                            <div className="col-span-1">
+                                <Image
+                                    src={imgClasesTwo}
+                                    alt={'Clases Uno'}
+                                    width={320}
+                                    height={320}
+                                />
+                            </div>
+                            <div className="col-span-1">
+                                <Image
+                                    src={imgClasesThree}
+                                    alt={'Clases Uno'}
+                                    width={320}
+                                    height={320}
+                                />
+                            </div>
+                        </div>
+
+                        <Image src={item02} alt={'Item Dos'} width={75} height={63} />
+                        <h4 className="font-manrope font-medium uppercase text-gris sm:mt-[20px] sm:text-[27px] sm:tracking-[0.25em]">
+                            {t('itemTwo')}
+                        </h4>
+
+                        <div className="sm:my-[80px] sm:grid sm:w-[700px] sm:grid-cols-2">
+                            <div className="flex items-center bg-[#f7f7f7] sm:col-span-1 sm:mr-[25px] sm:h-[430px] sm:flex-col sm:justify-center">
+                                <h3 className="font-manrope font-bold uppercase text-azul sm:text-[27px] sm:leading-[30px] sm:tracking-[0.2em]">
+                                    {t('promoOne')}
+                                </h3>
+                                <h2 className="font-marcellus font-normal text-verde sm:text-[50px]">
+                                    {t('priceOne')}
+                                </h2>
+                                <p className="text-center font-manrope font-normal text-gris sm:px-[35px] sm:pb-[50px] sm:pt-[30px] sm:text-[15px] sm:leading-[20px]">
+                                    {t('textOne')}
+                                </p>
+                                <h5 className="upercase font-manrope font-normal text-[#adadad] sm:pb-[30px] sm:text-[20px] sm:leading-[30px]">
+                                    {t('otherPriceOne')}
+                                </h5>
+                                <Link
+                                    href={'/'}
+                                    className="flex items-center justify-center rounded-[26px] border-[2px] border-azul bg-azul text-center font-marcellus font-normal text-blanco hover:opacity-50 sm:h-[53px] sm:w-[186px] sm:text-[23px]"
+                                >
+                                    {t('textBtn')}
+                                </Link>
+                            </div>
+                            <div className="flex items-center bg-[#f7f7f7] sm:col-span-1 sm:ml-[25px] sm:h-[430px] sm:flex-col sm:justify-center">
+                                <h3 className="font-manrope font-bold uppercase text-azul sm:text-[27px] sm:leading-[30px] sm:tracking-[0.2em]">
+                                    {t('promoTwo')}
+                                </h3>
+                                <h2 className="font-marcellus font-normal text-verde sm:text-[50px]">
+                                    {t('priceTwo')}
+                                </h2>
+                                <p className="text-center font-manrope font-normal text-gris sm:px-[35px] sm:py-[30px] sm:text-[15px] sm:leading-[20px]">
+                                    {t('textTwo')}
+                                </p>
+                                <h5 className="upercase font-manrope font-normal text-[#adadad] sm:pb-[30px] sm:text-[20px] sm:leading-[30px]">
+                                    {t('otherPriceTwo')}
+                                </h5>
+                                <Link
+                                    href={'/'}
+                                    className="flex items-center justify-center rounded-[26px] border-[2px] border-azul bg-azul text-center font-marcellus font-normal text-blanco hover:opacity-50 sm:h-[53px] sm:w-[186px] sm:text-[23px]"
+                                >
+                                    {t('textBtn')}
+                                </Link>
+                            </div>
+                        </div>
+
+                        <Image src={item03} alt={'Item Tres'} width={75} height={63} />
+                        <h4 className="font-manrope font-medium uppercase text-gris sm:mt-[20px] sm:text-[27px] sm:tracking-[0.25em]">
+                            {t('itemThree')}
+                        </h4>
+                        <div className="flex flex-col items-center justify-center sm:mt-[80px]">
+                            <Image src={imgEnjoy} alt="Enjoy" width={1092} height={545} />
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="bg-marron sm:py-[100px]">
+                <div className="container mx-auto">
+                    <div className="flex flex-col items-center justify-center sm:mb-[80px]">
+                        <h1 className="font-manrope font-normal text-blanco sm:text-[70px] sm:leading-[70px] sm:tracking-[0.02em]">
+                            FAQs
+                        </h1>
+                    </div>
+                    <FagsComponent />
                 </div>
             </section>
         </>
---
+++ src/components/Faqs/FaqsComponent.jsx
@@ -0,0 +1,66 @@
+import { useTranslations } from 'next-intl';
+
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from '@/components/ui/accordion';
+
+export default function FagsComponent() {
+    const t = useTranslations('OnDemandPage');
+
+    const faqs = [
+        {
+            index: 'item-1',
+            title: t('faqTitleOne'),
+            text: t('faqTextOne'),
+        },
+        {
+            index: 'item-2',
+            title: t('faqTitleTwo'),
+            text: t('faqTextTwo'),
+        },
+        {
+            index: 'item-3',
+            title: t('faqTitleThree'),
+            text: t('faqTextThree'),
+        },
+        {
+            index: 'item-4',
+            title: t('faqTitleFour'),
+            text: t('faqTextFour'),
+        },
+        {
+            index: 'item-5',
+            title: t('faqTitleFive'),
+            text: t('faqTextFive'),
+        },
+        {
+            index: 'item-6',
+            title: t('faqTitleSix'),
+            text: t('faqTextSix'),
+        },
+    ];
+
+    return (
+        <>
+            <Accordion type="single" collapsible className="w-full">
+                {faqs.map((faq, index) => (
+                    <AccordionItem
+                        key={faq.index}
+                        value={faq.index}
+                        className={index === 0 ? 'border-t' : ''}
+                    >
+                        <AccordionTrigger className="font-manrope font-bold text-blanco sm:text-[23px] sm:leading-[30px]">
+                            {faq.title}
+                        </AccordionTrigger>
+                        <AccordionContent className="font-manrope font-normal text-blanco sm:text-[15px] sm:leading-[20px]">
+                            {faq.text}
+                        </AccordionContent>
+                    </AccordionItem>
+                ))}
+            </Accordion>
+        </>
+    );
+}

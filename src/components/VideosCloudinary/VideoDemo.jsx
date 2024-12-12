e6d881bef725000ca62b732302175be77518fd57
Edgardo Ruotolo <edgardoruotolo@gmail.com>
2024-12-11 22:56:32 -0300

    Task: Correcciones Date: 11-12-2024 Version: 2.0

--- src/components/VideosCloudinary/VideoDemo.jsx
+++ src/components/VideosCloudinary/VideoDemo.jsx
@@ -11,6 +11,7 @@ const CldVideoPlayer = dynamic(() => import('next-cloudinary').then((mod) => mod
 export default function VideoDemo({ urlVideo }) {
     const uniqueId = useId();
     const [isMounted, setIsMounted] = useState(false);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         setIsMounted(true);
@@ -22,14 +23,21 @@ export default function VideoDemo({ urlVideo }) {
 
     return (
         <div className="video-container">
+            {error && <p>Error al cargar el video: {error.message}</p>}
             <CldVideoPlayer
                 id={`video-${uniqueId}`}
+                cloudName={process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}
+                apiKey={process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY}
+                apiSecret={process.env.NEXT_PUBLIC_CLOUDINARY_API_SECRET}
                 src={urlVideo}
                 width="1920"
                 height="1080"
                 className="absolute left-0 top-0 h-full w-full"
                 transformation={{
                     opacity: 80,
+                }}
+                onError={(event) => {
+                    setError(event.error);
                 }}
             />
         </div>
